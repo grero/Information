1784ed3f67804e60966e82e0a219d05663e11d9d
Roger Herikstad <roger.herikstad@gmail.com>
2013-09-27 12:32:57 +0800

    Added a basic README file

---
+++ README.mk
@@ -0,0 +1,5 @@
+###Introduction
+These tools were originally written as a way for me to get familiar with Julia, while hopefully ending up with something useful. Most of the code was originally written in MATLAB, and my main motivation for translating them into Julia was to measure any performance difference. I've used these codes in my analysis of neurophysiological data, but I've tried to keep them general enough to be applicable to any kind of data.
+
+
+##Getting started
